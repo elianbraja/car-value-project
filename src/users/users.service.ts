419113bf6ee9d694c49a8c3087a396387f3dc489
Elian Braja Personal <elianbraja@yahoo.com>
2023-01-12 15:51:13 +0100

    user CRUD operations

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { Repository } from "typeorm";
 import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "./user.entity";
@@ -9,9 +9,36 @@ export class UsersService {
     this.repo = repo;
   }
 
-  create(email: string, password: string){
-    const user =  this.repo.create({email, password});
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
 
     return this.repo.save(user);
+  }
+
+  findOne(id: number) {
+    // @ts-ignore
+    return this.repo.findOne({ where: { id } });
+  }
+
+  findAll(email: string) {
+    // @ts-ignore
+    return this.repo.find({ where: { email } });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException("user not found");
+    }
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException("user not found");
+    }
+    return this.repo.remove(user);
   }
 }
